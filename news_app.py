c8b83b12df5b76209e79ff5a6b737dad09040b20
Pablo Ramirez <105121715+bscifinance@users.noreply.github.com>
2023-03-14 17:58:13 -0300

    Update news_app.py

--- news_app.py
+++ news_app.py
@@ -17,20 +17,44 @@ from pathlib import Path
 import yaml
 from yaml.loader import SafeLoader
 
-st.set_page_config(page_title="BSCI_News", page_icon="logo_bsci.png")
+st.set_page_config(page_title="BSCI_News", page_icon="logo_bsci.png", layout="wide")
 
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
-def scrape_news(topic, language):
+def scrape_news(topic, period, language):
     # replace spaces with +
     topic = topic.replace(' ', '+')
     
-    if language == "Spanish":
+    if language == "Spanish" and period == "Última hora":
+        url = f'https://news.google.com/search?q={topic}+when:1h&hl=es-419&gl=CL&ceid=CL:es-419'
+    elif language == "Spanish" and period == "Últimas 24 horas":
+        url = f'https://news.google.com/search?q={topic}+when:1d&hl=es-419&gl=CL&ceid=CL:es-419'
+    elif language == "Spanish" and period == "Última semana":
+        url = f'https://news.google.com/search?q={topic}+when:7d&hl=es-419&gl=CL&ceid=CL:es-419'
+    elif language == "Spanish" and period == "Últimos 30 días":
+        url = f'https://news.google.com/search?q={topic}+when:1m&hl=es-419&gl=CL&ceid=CL:es-419'
+    elif language == "Spanish" and period == "Últimos seis meses":
+        url = f'https://news.google.com/search?q={topic}+when:6m&hl=es-419&gl=CL&ceid=CL:es-419'
+    elif language == "Spanish" and period == "Último año":
+        url = f'https://news.google.com/search?q={topic}+when:1y&hl=es-419&gl=CL&ceid=CL:es-419'
+    elif language == "Spanish" and period == "Cualquiera":
         url = f'https://news.google.com/search?q={topic}&hl=es-419&gl=CL&ceid=CL:es-419'
+    elif language == "English" and period == "Última hora":
+        url = f'https://news.google.com/search?q={topic}+when:1h&hl=en-US&gl=US&ceid=US:en'
+    elif language == "English" and period == "Últimas 24 horas":
+        url = f'https://news.google.com/search?q={topic}+when:1d&hl=en-US&gl=US&ceid=US:en'
+    elif language == "English" and period == "Última semana":
+        url = f'https://news.google.com/search?q={topic}+when:7d&hl=en-US&gl=US&ceid=US:en'
+    elif language == "English" and period == "Últimos 30 días":
+        url = f'https://news.google.com/search?q={topic}+when:1m&hl=en-US&gl=US&ceid=US:en'
+    elif language == "English" and period == "Últimos seis meses":
+        url = f'https://news.google.com/search?q={topic}+when:6m&hl=en-US&gl=US&ceid=US:en'
+    elif language == "English" and period == "Último año":
+        url = f'https://news.google.com/search?q={topic}+when:1y&hl=en-US&gl=US&ceid=US:en'
     else:
         url = f'https://news.google.com/search?q={topic}&hl=en-US&gl=US&ceid=US:en'
-   
+
     # scrape webpage
     page = requests.get(url).text
     soup = BeautifulSoup(page, 'html.parser')
@@ -40,7 +64,7 @@ def scrape_news(topic, language):
     title = [t.text for t in result_tl]
     
     result_dt = soup.select('[datetime]')
-    timedate = [d['datetime'] for d in result_dt]
+    timedate = [d.text for d in result_dt]
     
     result_src = soup.select('article .wEwyrc.AVN2gc.WfKKme')
     source = [s.text for s in result_src]
@@ -63,7 +87,7 @@ def scrape_news(topic, language):
 def summarize_news(df, num):
    url_list =  list(df["links"])[:num]
    news_list = []
-   for url, source in zip(url_list, df['source'][:num]):
+   for url, source, timedate in zip(url_list, df['source'][:num],df['timedate'][:num]):
    #for url in url_list:
       article = Article(url, keep_article_html=True)
       try:
@@ -75,7 +99,7 @@ def summarize_news(df, num):
       story = {
           'title': article.title.replace('\n',''),
           'source': source,
-          'date': article.publish_date,
+          'date': timedate,
           'link': url,
           'text': article.text.replace('\n',''),
           'summary': article.summary.replace('\n','').replace('.','. ')
@@ -128,6 +152,7 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+col1, col2 = st.columns([1, 3])
 
 if authentication_status == False:
     st.error("Username/password is incorrect")
@@ -138,38 +163,38 @@ if authentication_status == None:
 if authentication_status:
     log_activity(username)
 
-    authenticator.logout("Logout", "sidebar")
+    authenticator.logout("Logout", "main")
 
-    st.sidebar.image("logo_bsci.png", width=200)
-    st.sidebar.title("BSCI News")
-    st.sidebar.markdown("Resumen de noticias a tu medida")
-    st.sidebar.caption('''
-                **Credits**
-                - App desarrollada por bsci.finance'''
-                )
-    st.sidebar.markdown("Ingresa un tópico para buscar artículos de noticias y elige cuantos resumenes quieres desplegar:")
-    topic = st.sidebar.text_input("Tópico")
-    num_summaries = st.sidebar.slider("Número de resumenes a desplegar", 5, 20, 5)
-    language = st.sidebar.selectbox("Elige el Idioma", ["English", "Spanish"])
+    col1.image("logo_bsci.png", width=100)
+    col1.title("BSCI News")
+    col1.markdown("Resumen de noticias a tu medida")
+    # col1.caption('''
+    #             **Credits**
+    #             - App desarrollada por bsci.finance'''
+    #             )
+    col1.markdown("Ingresa un tópico para buscar artículos de noticias y elige cuantos resumenes quieres desplegar:")
+    topic = col1.text_input("Tópico")
+    num_summaries = col1.slider("Número de resúmenes a desplegar", 5, 20, 5)
+    selected_date = col1.selectbox("Elige la fecha", ["Cualquiera","Última hora", "Últimas 24 horas", "Última semana", "Últimos 30 días", "Últimos seis meses", "Último año"])
+    language = col1.selectbox("Elige el Idioma", ["English", "Spanish"])
     if language == 'English':
         lang = 'en'
     else:
         lang = 'es'
-    if st.sidebar.button("Search"):
-        if language == 'English':
-            df = scrape_news(topic, 'English')
-        else:
-            df = scrape_news(topic, 'Spanish')    
+    
+    if col1.button("Search"):
+        df = scrape_news(topic, language, selected_date)
+
         news_list = summarize_news(df, num_summaries)
-        st.write(f"Desplegando {len(news_list)} resumenes de noticias:")
+        col2.write(f"Desplegando {len(news_list)} resumenes de noticias:")
 
         for i, news in enumerate(news_list):
-            st.write(f"Title: [{news['title']}]({news['link']})")
-            st.write(f"Source: {news['source']}")
-            st.write(f"Time and Date: {news['date']}")
-            st.write(f"Summary: {news['summary']}")
-            st.write("----")
-            st.audio(play_audio(news['summary'], lang), format='audio/mp3')
+            col2.write(f"Title: [{news['title']}]({news['link']})")
+            col2.write(f"Source: {news['source']}")
+            col2.write(f"Time and Date: {news['date']}")
+            col2.write(f"Summary: {news['summary']}")
+            col2.write("----")
+            col2.audio(play_audio(news['summary'], lang), format='audio/mp3')
 
 
 
